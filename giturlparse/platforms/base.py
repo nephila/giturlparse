c6a4b4cf06f5d94dd4590a823890e6937e6b2805
nooperpudd <365504029@qq.com>
2016-12-26 14:28:22 +0800

    fix https url
    fix https url

--- giturlparse/platforms/base.py
+++ giturlparse/platforms/base.py
@@ -5,14 +5,14 @@ class BasePlatform(object):
     FORMATS = {
         'ssh': r"%(_user)s@%(host)s:%(repo)s.git",
         'http': r"http://%(host)s/%(repo)s.git",
-        'https': r"http://%(host)s/%(repo)s.git",
+        'https': r"https://%(host)s/%(repo)s.git",
         'git': r"git://%(host)s/%(repo)s.git"
     }
 
     PATTERNS = {
         'ssh': r"(?P<_user>.+)s@(?P<domain>.+)s:(?P<repo>.+)s.git",
         'http': r"http://(?P<domain>.+)s/(?P<repo>.+)s.git",
-        'https': r"http://(?P<domain>.+)s/(?P<repo>.+)s.git",
+        'https': r"https://(?P<domain>.+)s/(?P<repo>.+)s.git",
         'git': r"git://(?P<domain>.+)s/(?P<repo>.+)s.git"
     }
 
