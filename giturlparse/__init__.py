a29728dbe3e163df3524f8f6e5627ac458a250ec
Iacopo Spalletti <i.spalletti@nephila.it>
2018-01-20 09:49:23 +0100

    Release 0.9

--- giturlparse/__init__.py
+++ giturlparse/__init__.py
@@ -6,7 +6,7 @@ from .result import GitUrlParsed
 
 __author__ = 'Iacopo Spalletti'
 __email__ = 'i.spalletti@nephila.it'
-__version__ = '0.9.dev1'
+__version__ = '0.9'
 
 
 def parse(url, check_domain=True):
